c45eae9ab5292091c1e54fb0a9259a479c85ca07
abrahimsamir738-blip <abrahimsamir738-blip@gmai.com>
2026-06-20 13:00:24 -0700

    dd

--- services/api.ts
+++ services/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || ' http://localhost:8000/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || ' https://tracie-rarefactive-evia.ngrok-free.dev/api';
 
 // Create axios instance for website (no auth required)
 const api: AxiosInstance = axios.create({
